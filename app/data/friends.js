0880822caf3e182275657165fa15b48efcb9b63b
Tamar Cabayan <tcabayan@gmail.com>
2020-05-09 16:30:08 -0700

    Uploading files

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Miley',
+		photo: 'https://www.biography.com/.image/t_share/MTQyMDA2MDcxOTA3NTkxMjMw/miley-cyrus_gettyimages-605936368jpg.jpg',
+		scores: [
+			'5',
+			'1',
+			'5',
+			'5',
+			'5',
+			'2',
+			'3',
+			'5',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'Liam',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMTQ5ODI0MDc4M15BMl5BanBnXkFtZTgwNTM5MDk3MTE@._V1_UY1200_CR84,0,630,1200_AL_.jpg',
+		scores: [
+			'2',
+			'4',
+			'3',
+			'2',
+			'5',
+			'3',
+			'3',
+			'2',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'Brad',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Brad_Pitt_2019_by_Glenn_Francis.jpg',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'1',
+			'1',
+			'1',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Jennifer',
+		photo: 'https://media1.popsugar-assets.com/files/thumbor/ptdgPx5tCvvD9kUsU7pQFMUkBIA/207x134:1865x1792/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/09/09/028/n/1922398/066318895d76e2ef0c31d8.46065434_/i/Jennifer-Aniston.jpg',
+		scores: [
+			'3',
+			'4',
+			'5',
+			'3',
+			'3',
+			'3',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Angelina',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/a/ad/Angelina_Jolie_2_June_2014_%28cropped%29.jpg',
+		scores: [
+			'5',
+			'1',
+			'1',
+			'1',
+			'1',
+			'3',
+			'5',
+			'1',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Leo',
+		photo: 'https://m.media-amazon.com/images/M/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_UY1200_CR130,0,630,1200_AL_.jpg',
+		scores: [
+			'5',
+			'1',
+			'5',
+			'2',
+			'4',
+			'4',
+			'5',
+			'3',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kate',
+		photo: 'https://www.biography.com/.image/t_share/MTE1ODA0OTcxODI4MjgyODkz/kate-winslet-9534599-1-402.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'3',
+			'4',
+			'4',
+			'2',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Giselle',
+		photo: 'https://www.biography.com/.image/t_share/MTE4MDAzNDEwODYzODE4MjU0/alexander-wang---front-row--backstage---fall-2012-mercedes-benz-fashion-week.jpg',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Tom',
+		photo: 'https://static.nfl.com/static/content/public/static/img/fantasy/transparent/512x512/BRA371156.png',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'5'
+		]
+	},
+	{
+		name: 'Robert',
+		photo: 'https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzMzODYwMzY2/robert-downey-jr-9542052-1-402.jpg',
+		scores: [
+			'5',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
